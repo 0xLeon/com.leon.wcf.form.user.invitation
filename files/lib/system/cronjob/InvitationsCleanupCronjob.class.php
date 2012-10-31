c99a4a6f16663f0e4281ba576f586f26f21d2903
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-10-31 14:25:32 +0100

    removed useless requirement

--- files/lib/system/cronjob/InvitationsCleanupCronjob.class.php
+++ files/lib/system/cronjob/InvitationsCleanupCronjob.class.php
@@ -1,7 +1,6 @@
 <?php
 // wcf imports
 require_once(WCF_DIR.'lib/data/cronjobs/Cronjob.class.php');
-require_once(WCF_DIR.'lib/data/user/invitation/InvitationList.class.php');
 
 /**
  * Deletes accepted invitations with no user associated.
